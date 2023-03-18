95cc194d0ff3f860a077ff580434553528b1ea67
AwesomeWithRex <84873608+AwesomeRex7@users.noreply.github.com>
2023-03-18 16:22:45 -0400

    Release 0.01
    First public release

---
+++ conf.lua
@@ -0,0 +1,13 @@
+function love.conf(t)
+t.window.width = 300
+t.window.height = 388
+t.modules.joystick = false
+t.modules.physics = false
+t.window.title = "Flappy Box"
+t.window.icon = "icon.png"
+t.window.resizable = false
+t.console = false
+t.version = "11.4"
+t.modules.graphics = true
+t.modules.audio = true
+end
